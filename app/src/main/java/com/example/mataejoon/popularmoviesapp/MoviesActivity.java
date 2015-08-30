b93c690c2adcd1b69b8b05c9838308389d13f021
Mathieu <tan.mathieu@gmail.com>
2015-08-30 17:51:03 +0200

    dummy list view

--- app/src/main/java/com/example/mataejoon/popularmoviesapp/MoviesActivity.java
+++ app/src/main/java/com/example/mataejoon/popularmoviesapp/MoviesActivity.java
@@ -4,14 +4,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 
 public class MoviesActivity extends AppCompatActivity {
+
+    private ArrayAdapter<String>    mMoviesAdapter;
+    private ListView                mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+
+        fetchView();
+        setupListView();
+    }
+
+    private void fetchView() {
+        mListView = (ListView)findViewById(R.id.movies_listview);
+    }
+
+    private void setupListView() {
+        mListView.setAdapter(getAdapter());
     }
 
     @Override
@@ -34,5 +52,26 @@ public class MoviesActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private ArrayAdapter<String> getAdapter() {
+        mMoviesAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                fetchDatas()
+        );
+
+        return mMoviesAdapter;
+    }
+
+    private String[] fetchDatas() {
+        String [] datas = {
+                "Jurasic Park",
+                "Very Bad Trip",
+                "Forest Gump",
+                "Whatever :)"
+        };
+
+        return datas;
     }
 }
