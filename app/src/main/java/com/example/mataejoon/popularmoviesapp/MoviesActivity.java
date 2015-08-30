f04ef86ef6f2706bfe6f54ab0ed0aab20f22e5cc
Mathieu <tan.mathieu@gmail.com>
2015-08-30 17:20:52 +0200

    udpated activity from using actionbar to appcompat

--- app/src/main/java/com/example/mataejoon/popularmoviesapp/MoviesActivity.java
+++ app/src/main/java/com/example/mataejoon/popularmoviesapp/MoviesActivity.java
@@ -1,12 +1,12 @@
 package com.example.mataejoon.popularmoviesapp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MoviesActivity extends ActionBarActivity {
+public class MoviesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
